d91d8ed6257f207dfd532a909add016618d58c38
Administrator <Administrator@SKY-20150724IXD>
2016-05-03 22:22:43 +0800

    提交进度

--- src/main/java/org/core/util/StringUtils.java
+++ src/main/java/org/core/util/StringUtils.java
@@ -1,5 +1,10 @@
 package org.core.util;
 
 public class StringUtils {
+	public static void main(String[] args) {
+		String hello = "Hello";
+		int codePointCount = hello.codePointCount(1, 4);
+		System.out.println(codePointCount);
 
+	}
 }
---
+++ src/test/java/org/core/test/InputTest.java
@@ -0,0 +1,15 @@
+package org.core.test;
+
+import java.util.Date;
+
+public class InputTest {
+	public static void main(String[] args) {
+		// Scanner scanner = new Scanner(System.in);
+		// String line = scanner.nextLine();
+		// System.out.println(line);
+
+//		Console console = System.console();
+
+		System.out.printf("%tc --- tc", new Date());
+	}
+}
